dd6018bcd7ee2f380f6846dec5780d35e6ac222d
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-06-19 20:57:16 +0900

    add parse

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     fs::{self, File},
     io::Write,
     path::Path,
-    process::{Command, Stdio},
+    process::{self, Command, Stdio},
 };
 
 const TSCONFIG_PATH: &str = "tmpTsconfig.json";
@@ -25,21 +25,49 @@ fn tsc() {
         .expect("tsc error");
 }
 
-fn write_tsconfig(paths: Vec<String>) -> std::io::Result<()> {
-    let json = format!(
-        "{}{}{}",
-        "{ \"extends\": \"./tsconfig\", \"include\": [\"",
-        paths.join("\", \""),
-        "\"] }"
-    );
+fn write_tsconfig(paths: Vec<String>, isDeclaration: bool) -> std::io::Result<()> {
+    let json = if isDeclaration {
+        format!(
+            r#"{{ "extends": "./tsconfig", "emitDeclarationOnly": true, "include": ["{}"] }}"#,
+            paths.join("\", \"")
+        )
+    } else {
+        format!(
+            r#"{{ "extends": "./tsconfig", "include": ["{}"] }}"#,
+            paths.join("\", \"")
+        )
+    };
     write(json, &Path::new(TSCONFIG_PATH))
 }
 fn remove_tsconfig() {
     fs::remove_file(TSCONFIG_PATH).expect("remove tsconfig error");
 }
 fn main() {
-    let files: Vec<String> = std::env::args().collect();
-    write_tsconfig(files).expect("write tsconfig error");
+    let mut files: Vec<String> = Vec::new();
+    let mut isDeclaration = true;
+    for arg in std::env::args() {
+        if arg.starts_with("-") {
+            for char in arg.chars().skip(1) {
+                match char {
+                    'd' => {
+                        // declaration
+                        isDeclaration = true;
+                    }
+                    'e' => {
+                        // emit
+                        isDeclaration = false;
+                    }
+                    _ => {
+                        eprintln!("unknown command option \"{}\"", char);
+                        process::exit(1);
+                    }
+                }
+            }
+        } else {
+            files.push(arg);
+        }
+    }
+    write_tsconfig(files, isDeclaration).expect("write tsconfig error");
     tsc();
     remove_tsconfig();
 }
